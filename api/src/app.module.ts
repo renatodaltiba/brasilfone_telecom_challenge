976378146704f4a6616dbf949d0dcf7f65a18c37
Renato D. Schneider <rdaltibaschneider@gmail.com>
2022-05-22 15:22:07 -0400

    chore: remove class-validator

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersResolver } from './users/users.resolver';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [PrismaModule, UsersModule],
   controllers: [],
-  providers: [UsersResolver],
+  providers: [],
 })
 export class AppModule {}
